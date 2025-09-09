e92a049784eebc2fadb6331094aac55182aeaf8e
Aditi Maheshwari <78598563+Aditi9183@users.noreply.github.com>
2025-09-09 19:08:57 +0530

    Add files via upload

---
+++ Pattern9.cpp
@@ -0,0 +1,28 @@
+/*
+1 2 3 4 5
+2 4 6 8 10
+3 6 9 12 15
+4 8 12 16 20
+5 10 15 20 25
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        int k=i;
+        for(int j=1;j<=n;j++)
+        {
+            cout<<k<<" ";
+            k+=i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
